5725dfad24abc5c353ec34f81e8094dcd1e16b0a
newlax <qwegornikitin@gmail.com>
2024-05-02 13:56:04 +0300

    Добавьте файлы проекта.

---
+++ GeoGraphs/Landing.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GeoGraphs
+{
+    public partial class Landing : Page
+    {
+        public Landing()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            var ClickedButton = e.OriginalSource as NavButton;
+            NavigationService.Navigate(ClickedButton.NavUri);
+        }
+    }
+}
